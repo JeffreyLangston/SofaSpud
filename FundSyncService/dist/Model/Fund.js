87c6bd36d356e04fbb7fd4218b1b2d106400002d
Jeff Langston <info@jefflangston.ca>
2018-10-23 08:36:41 -0700

    removed old fund sync project.

--- FundSyncService/dist/Model/Fund.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Fund {
-    constructor(symbol) {
-        this.Symbol = symbol;
-    }
-}
-exports.Fund = Fund;
-//# sourceMappingURL=Fund.js.map
--- app/src/components/FundGrid/FundGrid.jsx
+++ app/src/components/FundGrid/FundGrid.jsx
@@ -2,6 +2,18 @@ import React, { Component } from "react";
 
 class FundGrid extends Component {
   render() {
-    return ();
+    return (
+
+      <div>
+      {
+        if(this.props.uid){
+      foreach(row in this.props.rows){
+        <div>row</div>
+      }
+      }
+    }
+      </div>
+
+    );
   }
 }
